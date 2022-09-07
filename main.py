8c3b2e86208180d0be0f557b7529b2f09def4ff1
Ivan Cafiero <ivancafiero@gmail.com>
2022-09-07 11:32:07 +0200

    [CI] Maybe this time will work

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ for codec in codecs:
     command = f'find Resources -type d -not -name {codec} -not -name PinConfigs.kext -not -path Resources/PinConfigs.kext/Contents -not -name Resources | xargs rm -rf {{}};'
     subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     subprocess.run(['xcodebuild', '-project', 'AppleALC.xcodeproj', '-scheme', 'AppleALC', '-configuration', 'Release', '-sdk', 'macosx', '-derivedDataPath', 'out'], capture_output=not args.verbose)
-    subprocess.run(['mv', 'out/Build/Products/Release/AppleALC-1.7.4-RELEASE.zip', f'../Kexts/{codec}.zip'], capture_output=not args.verbose)
+    subprocess.run(['mv', 'AppleALC/out/Build/Products/Release/AppleALC-1.7.4-RELEASE.zip', f'../Kexts/{codec}.zip'], capture_output=not args.verbose)
     subprocess.run(['git', 'reset', '--hard', 'HEAD'], capture_output=not args.verbose)
     #subprocess.run(['rm', '-rf', 'out'], capture_output=not args.verbose)
     sys.exit()
