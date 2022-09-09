ed327a84cb009533199aa7d3e3fadf0cb4879b17
Ivan Cafiero <ivancafiero@gmail.com>
2022-09-09 02:51:56 +0200

    [Docs] Fixed sorting

--- .assets/scripts/layout_id_finder.py
+++ .assets/scripts/layout_id_finder.py
@@ -2,24 +2,33 @@ import os
 import plistlib
 import json
 
-from pprint import pprint
-#1 cd onto Resources
 
-def find_layout_ids(codec_name: str):
+def find_layout_ids():
 
     os.chdir('Resources')
 
-    codecs = [codec for codec in os.listdir(os.getcwd()) if not codec.endswith('plist') and not codec.endswith('kext') and codec != '.DS_Store']
+    codecs = [codec for codec in os.listdir(os.getcwd()) if not codec.endswith(
+        'plist') and not codec.endswith('kext') and codec != '.DS_Store']
     final_output = {}
+
     for codec in codecs:
         final_output[codec] = {}
+        layout_list = []
+
         with open(os.path.join(codec, 'Info.plist'), 'rb') as fp:
             pl = plistlib.load(fp)
         for layout in pl['Files']['Layouts']:
-            final_output[codec][layout['Id']] = layout['Comment'] if 'Comment' in layout.keys() else 'No description available'
+            layout_list.append(str(layout['Id']))
+            layout_list = sorted(layout_list, key=int)
+
+        for layout_id in layout_list:
+            final_output[codec][layout_id] = ""
+        for layout in pl['Files']['Layouts']:
+            final_output[codec][str(layout['Id'])] = layout['Comment'] if 'Comment' in layout.keys(
+            ) else 'No description available'
 
     return final_output
 
 
 with open('codecs.json', 'w') as fp:
-    json.dump(find_layout_ids('CX20561'), fp, indent=4)
+    json.dump(find_layout_ids(), fp, indent=4)
